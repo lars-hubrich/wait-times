119c9ea3d2e2cf13daf45a042faa70a434b8c712
Lars Hubrich <github@hubi4.de>
2025-06-15 01:46:20 +0200

    better offline

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+      .then(() => self.skipWaiting())
   );
 });
 
@@ -20,41 +20,15 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
       )
-    ).then(self.clients.claim())
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
-  const url = event.request.url;
-  if (url.includes('allorigins.win')) {
-    event.respondWith((async () => {
-      try {
-        const networkRes = await fetch(event.request);
-        const cache = await caches.open(CACHE_NAME);
-        cache.put(event.request, networkRes.clone());
-        return networkRes;
-      } catch (err) {
-        const cachedRes = await caches.match(event.request);
-        if (!cachedRes) throw err;
-        const blob = await cachedRes.blob();
-        const headers = new Headers(cachedRes.headers);
-        headers.set('X-From-Cache', 'true');
-        return new Response(blob, {
-          status:    cachedRes.status,
-          statusText:cachedRes.statusText,
-          headers
-        });
-      }
-    })());
-    return;
-  }
-
   event.respondWith(
     caches.match(event.request)
-      .then(resp => resp || fetch(event.request))
+      .then(cached => cached || fetch(event.request))
   );
 });
